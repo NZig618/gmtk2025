77130e091df8ab2c09862f30f90faa435295210b
NZig618 <94143570+NZig618@users.noreply.github.com>
2025-08-01 21:48:44 -0400

    [Implementation] Created attack functionality

---
+++ Assets/Scripts/BulletLvl2.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BulletLvl2 : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
